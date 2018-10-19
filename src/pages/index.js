2988b5a372ee6d5d391e8de0f7a1596eb5a2531f
Tony Tang <tony.cn@outlook.com>
2018-10-19 14:14:26 +0800

    Initial commit :tada:

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi</h1>
+    <p>This is Tony Tang, a Solution Architect and Developer.</p>
+  </Layout>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const SecondPage = () => (
+  <Layout>
+    <p>page intentionally left blank</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
